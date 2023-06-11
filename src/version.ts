3f0c0e001d504eb8004a267e60a0716a6a179206
Marco Acierno <marcoaciernoemail@gmail.com>
2023-06-11 17:49:52 +0200

    3.10

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
-import { NowBuildError } from '@vercel/build-utils';
+import { NowBuildError } from "@vercel/build-utils";
 
 interface PythonVersion {
   version: string;
@@ -11,27 +11,33 @@ interface PythonVersion {
 // The order must be most recent first
 const allOptions: PythonVersion[] = [
   {
-    version: '3.9',
-    pipPath: 'pip3.9',
-    pythonPath: 'python3.9',
-    runtime: 'python3.9',
+    version: "3.10",
+    pipPath: "pip3.10",
+    pythonPath: "python3.10",
+    runtime: "python3.10",
   },
   {
-    version: '3.6',
-    pipPath: 'pip3.6',
-    pythonPath: 'python3.6',
-    runtime: 'python3.6',
-    discontinueDate: new Date('2022-07-18'),
+    version: "3.9",
+    pipPath: "pip3.9",
+    pythonPath: "python3.9",
+    runtime: "python3.9",
+  },
+  {
+    version: "3.6",
+    pipPath: "pip3.6",
+    pythonPath: "python3.6",
+    runtime: "python3.6",
+    discontinueDate: new Date("2022-07-18"),
   },
 ];
 
 function getDevPythonVersion(): PythonVersion {
   // Use the system-installed version of `python3` when running `vercel dev`
   return {
-    version: '3',
-    pipPath: 'pip3',
-    pythonPath: 'python3',
-    runtime: 'python3',
+    version: "3",
+    pipPath: "pip3",
+    pythonPath: "python3",
+    runtime: "python3",
   };
 }
 export function getLatestPythonVersion({
@@ -57,8 +63,8 @@ export function getSupportedPythonVersion({
   }
   let selection = getLatestPythonVersion({ isDev: false });
 
-  if (typeof pipLockPythonVersion === 'string') {
-    const found = allOptions.find(o => o.version === pipLockPythonVersion);
+  if (typeof pipLockPythonVersion === "string") {
+    const found = allOptions.find((o) => o.version === pipLockPythonVersion);
     if (found) {
       selection = found;
     } else {
@@ -70,14 +76,14 @@ export function getSupportedPythonVersion({
 
   if (isDiscontinued(selection)) {
     throw new NowBuildError({
-      code: 'BUILD_UTILS_PYTHON_VERSION_DISCONTINUED',
-      link: 'http://vercel.link/python-version',
+      code: "BUILD_UTILS_PYTHON_VERSION_DISCONTINUED",
+      link: "http://vercel.link/python-version",
       message: `Python version "${selection.version}" detected in Pipfile.lock is discontinued and must be upgraded.`,
     });
   }
 
   if (selection.discontinueDate) {
-    const d = selection.discontinueDate.toISOString().split('T')[0];
+    const d = selection.discontinueDate.toISOString().split("T")[0];
     console.warn(
       `Error: Python version "${selection.version}" detected in Pipfile.lock has reached End-of-Life. Deployments created on or after ${d} will fail to build. http://vercel.link/python-version`
     );
